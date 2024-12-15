bd2e1108f04b256189d1d0c36e4aeb01b64ef95e
ksbickmore <12804658+ksbickmore@users.noreply.github.com>
2024-12-14 20:03:29 -0500

    update stuff

---
+++ src/robot.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void robot_speak(const char *message){
+    printf("%s", message);
+}
+
+void robot_greet(){
+    printf("Hello, I am a robot\n");
+}
+void robot_farewell(){
+    printf("Goodbye, I am a robot\n");
+}
